f42df630f039587202c511e7a746b77cc9662d6e
Riiel-edu <riven18@farmingdale.edu>
2025-03-03 12:16:03 -0500

    Finished Card Class

--- src/main/java/com/example/csc_311_individual_assignment_cards/Card.java
+++ src/main/java/com/example/csc_311_individual_assignment_cards/Card.java
@@ -1,4 +1,61 @@
 package com.example.csc_311_individual_assignment_cards;
 
+import javafx.scene.image.Image;
+
 public class Card  {
+
+    private int value;
+    private Image image;
+
+    /**
+     * Default constructor for the Card class
+     */
+    public Card() {
+        image = null;
+        value = 0;
+    }
+
+    /**
+     * Parameterized constructor for the card class, assigns value to the image and card number value
+     * @param image the image of the card
+     * @param value the value of the card
+     */
+    public Card(Image image, int value) {
+        this.image = image;
+        this.value = value;
+    }
+
+    /**
+     * Getter method for image param
+     * @return the image of the card
+     */
+    public Image getImage() {
+        return this.image;
+    }
+
+    /**
+     * Setter for the image param
+     * @param image the new image
+     */
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    /**
+     * Getter method for value param
+     * @return the value of the card
+     */
+    public int getValue() {
+        return this.value;
+    }
+
+    /**
+     * Setter for the value param
+     * @param value the new value
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+
 }
